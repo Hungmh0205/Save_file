db13f72324439500e4f32281b8d87d3c9fe8d924
Mai Hữu Hùng <maihuuhung0205@gmail.com>
2025-06-11 11:48:15 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,1 @@
- // This will totally prevent us from loading evil URLs!
-      if (url.match(/^https?:\/\//)) {
-        setInnerText(document.getElementById("log"),
-          "Sorry, cannot load a URL containing \"http\".");
-        return;
-      }
+<script>alert(1)</script>
